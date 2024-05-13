bac4556fee7ab66783e61005a171ed0a682aa120
msi <meibrohimov.@gmail.com>
2024-05-13 10:31:46 +0500

    change ListView

--- root/apps/urls.py
+++ root/apps/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home, UserList
+from .views import Home, UserList, UsersDelete
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
-    path('user_list', UserList.as_view(), name="user_list")
+    path('user_list', UserList.as_view(), name="user_list"),
+    path('/<int:pk>', UsersDelete.as_view(), name='delete'),
 ]
--- root/apps/views.py
+++ root/apps/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DeleteView
 from .models import People
 
 
@@ -10,3 +10,17 @@ class UserList(ListView):
     model = People
     context_object_name = 'users'
     template_name = 'user_list.html'
+
+
+class UsersDelete(DeleteView):
+    model = People
+    context_object_name = 'users'
+    pk_url_kwarg = 'pk'
+    success_url = '/'
+    template_name = 'user_list.html'
+
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
